0777fb6154bd2b439c5b8a64479d2fe443deeb94
Jeff Moore <jmoore@jeff-moores-computer-2.local>
2020-05-06 21:35:04 -0400

    fixed broken url v7

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Jeff'
+        name: 'Jeff Moore'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Jeff'
+        name: 'Jeff Moore'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Jeff'
+        name: 'Jeff Moore'
     })
 })
 
@@ -84,7 +84,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Jeff',
+        name: 'Jeff Moore',
         errorMessage: 'Help article not found.'
     })
 })
@@ -92,7 +92,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Jeff',
+        name: 'Jeff Moore',
         errorMessage: 'Page not found.'
     })
 })
